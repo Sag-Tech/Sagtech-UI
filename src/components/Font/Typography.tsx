bc945969b2f26940c39534273e536ab5c9034548
MarynaDanilkina <Danilkinamari99@gmail.com>
2023-07-12 13:53:05 +0300

    feat: add color and line

--- src/components/Font/Typography.tsx
+++ src/components/Font/Typography.tsx
@@ -6,10 +6,10 @@ interface TypographyTypes {
   text?: string
   tag?: 'h1' | 'h2' | 'h3' | 'h4' | 'p'
   type?: 'Body L' | 'Body S' | 'Labels' | 'Info' | 'Buttons'
+  color?: 'text-black_1' | 'text-black_2' | 'text-black_3' | 'text-black_4' | 'text-grey_1' | 'text-grey_2' | 'text-grey_3' | 'text-grey_4' | 'text-white_1' | 'text-white_2' | 'text-white_3' | 'text-white_4' | 'text-white' | 'text-pr_purple' | 'text-sec_purple' | 'text-pr_blue' | 'text-sec_blue' | 'text-error' | 'text-warning' | 'text-success'
 }
-const color = 'text-black_1'
 
-const Typography: React.FC<TypographyTypes> = ({ text, tag: ElementTag = 'p' as keyof JSX.IntrinsicElements, type = 'Body L' }) => {
+const Typography: React.FC<TypographyTypes> = ({ text, tag: ElementTag = 'p' as keyof JSX.IntrinsicElements, type = 'Body L', color = 'text-black_1' }) => {
   const tagDefault = ElementTag === 'p'
   const fontClasses = classNames({
     [typographyConst.h1]: ElementTag === 'h1',
@@ -20,12 +20,13 @@ const Typography: React.FC<TypographyTypes> = ({ text, tag: ElementTag = 'p' as 
     [typographyConst.bodyS]: type === 'Body S' && tagDefault,
     [typographyConst.labels]: type === 'Labels' && tagDefault,
     [typographyConst.info]: type === 'Info' && tagDefault,
-    [typographyConst.buttons]: type === 'Buttons' && tagDefault
+    [typographyConst.buttons]: type === 'Buttons' && tagDefault,
+    [color]: true
   })
 
   return (
     <ElementTag
-        className={`${fontClasses} ${color}`}
+      className={`${fontClasses}`}
     >
         {text}
     </ElementTag>
