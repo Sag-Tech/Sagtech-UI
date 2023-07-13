3c92b59dfd748c97e1665b747b9f22179aef37f9
MarynaDanilkina <Danilkinamari99@gmail.com>
2023-07-13 09:33:17 +0300

    fix: fix Typo

--- src/components/Typo/Typography.stories.tsx
+++ src/components/Typo/Typography.stories.tsx
@@ -11,5 +11,5 @@ const Template: StoryFn = (arg) => <Typography {...arg} />
 export const Default = Template.bind({})
 Default.args = {
   text: 'Example',
-  tag: 'h1'
+  tag: 'p'
 }
---
+++ src/components/Typo/Typography.tsx
@@ -0,0 +1,48 @@
+import classNames from 'classnames'
+import React, { useMemo } from 'react'
+import type { VariantTypoTagsStyles, VariantTypoColors, VariantTypoTags } from './types'
+import typographyConst from './TypographyConst/TypographyConst'
+
+export interface TypographyTypes {
+  text?: string
+  tag?: VariantTypoTags
+  type?: VariantTypoTagsStyles
+  color?: VariantTypoColors
+}
+
+const Typography: React.FC<TypographyTypes> = ({ text, tag = 'p', type = 'BodyL', color = 'black_1' }) => {
+  const tagDefault = tag === 'p'
+  const Component = useMemo(() => tag, [tag])
+  const classes = useMemo(
+    () =>
+      classNames(
+        {
+          [typographyConst.h1]: tag === 'h1',
+          [typographyConst.h2]: tag === 'h2',
+          [typographyConst.h3]: tag === 'h3',
+          [typographyConst.h4]: tag === 'h4',
+          [typographyConst.bodyL]: type === 'BodyL' && tagDefault,
+          [typographyConst.bodyS]: type === 'BodyS' && tagDefault,
+          [typographyConst.labels]: type === 'Labels' && tagDefault,
+          [typographyConst.info]: type === 'Info' && tagDefault,
+          [typographyConst.buttons]: type === 'Buttons' && tagDefault,
+          [`text-${color}`]: true
+        }
+      ),
+    [
+      type,
+      tag,
+      tagDefault,
+      color
+    ]
+  )
+
+  return (
+    <Component
+      className={`${classes}`}
+    >
+        {text}
+    </Component>
+  )
+}
+export default Typography
