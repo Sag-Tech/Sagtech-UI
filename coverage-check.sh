cfd61dac94641e6d97308cd8410f915ca3b7b78d
Andrew <andrew@sagtech.io>
2023-07-15 13:22:44 +0200

    [FEAT]: added coverage scripts

--- coverage-check.sh
+++ coverage-check.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-jest --coverage --coverageReporters=text-summary | tee coverage.txt
-coverage=$(grep 'All files' coverage.txt | awk '{print $4}' | sed 's/%//')
+mkdir -p reports
+current_time=$(date "+%Y.%m.%d-%H.%M.%S")
+npx jest --coverage --coverageReporters=text-summary | tee "reports/coverage-$current_time.txt"
+coverage=$(grep 'All files' "reports/coverage-$current_time.txt" | awk '{print $4}' | sed 's/%//')
 threshold=90
 if (( $(echo "$coverage < $threshold" | bc -l) )); then
   echo "Code coverage is below $threshold%"
