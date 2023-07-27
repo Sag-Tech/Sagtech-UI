8acd7c61e975ce0085da8932dd077324642ec832
Ivan <spell20044@gmail.com>
2023-07-28 01:19:30 +0300

    feat:change animation button

--- src/components/AnimationButton/AnimationButton.tsx
+++ src/components/AnimationButton/AnimationButton.tsx
@@ -1,30 +1,40 @@
-import React, { type ButtonHTMLAttributes, type DetailedHTMLProps } from 'react'
+import React, {
+  type ButtonHTMLAttributes,
+  type DetailedHTMLProps
+} from 'react'
 import '../../styles/globals.css'
 import { Icon } from '@components/IconComponent/Icon'
 
-interface AnimateType extends DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> {
+interface AnimateType
+  extends DetailedHTMLProps<
+  ButtonHTMLAttributes<HTMLButtonElement>,
+  HTMLButtonElement
+  > {
   text?: string
 }
 
-const AnimationButton: React.FC<AnimateType> = ({ text = 'Some txt', ...rest }) => {
+const AnimationButton: React.FC<AnimateType> = ({
+  text = 'Some txt',
+  ...rest
+}) => {
   return (
-   <div className='inline-block'>
-      <button
+    <div className="inline-block">
+    <button
+      type="button"
       {...rest}
-      className='buttonAnim  bg-pr_purple  items-center rounded-circle buttonAnims overflow-hidden'
+      className="buttonAnim  buttonAnims  items-center overflow-hidden rounded-circle bg-pr_purple"
     >
-     <div className='flex items-center '>
-     <span
-        className='text-white  whitespace-nowrap animation font-["Manrope"] font-bold leading-24'
-      >
-        {text}
-      </span>
-      <div role='contentinfo' className='iconStyles'>
-      <Icon icon='arrow' size={32} viewBox='0 0 32 32' color='#6D3EF1' />
+      <div className="flex items-center ">
+        <span className='animation  whitespace-nowrap font-["Manrope"] font-bold leading-24 text-white'>
+          {text}
+        </span>
+        <div role="contentinfo" className="iconStyles">
+          <Icon icon="arrow" size={32} viewBox="0 0 32 32" color="#6D3EF1" />
+        </div>
       </div>
-     </div>
     </button>
-   </div>
+  </div>
+
   )
 }
 
