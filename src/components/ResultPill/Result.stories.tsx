7116aebf496e5d1bc5991aaa5d24b2ab382d7d2e
Ivan <spell20044@gmail.com>
2023-08-01 15:25:12 +0300

    feat: avatar card and result pill

---
+++ src/components/ResultPill/Result.stories.tsx
@@ -0,0 +1,16 @@
+import type { StoryFn, Meta } from '@storybook/react'
+import Result from './Result'
+
+const meta: Meta<typeof Result> = {
+  title: 'Result',
+  component: Result
+}
+export default meta
+
+const Template: StoryFn = (arg) => <Result {...arg} />
+
+export const ResultComponent = Template.bind({})
+ResultComponent.args = {
+  title: '+27000',
+  info: 'new exclusive users'
+}
